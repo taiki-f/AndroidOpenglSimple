8cc5ddf9e87e36cff3003b5765553d0b65f61875
taiki <matsudalvup@gmail.com>
2020-10-16 22:03:11 +0900

    タイマークラス実装

--- NativeLibrary/Src/System/Timer.cpp
+++ NativeLibrary/Src/System/Timer.cpp
@@ -1,1 +1,113 @@
 #include "Timer.hpp"
+#include <time.h>
+
+namespace drs { namespace time {
+
+    /// <summary>
+    /// PImplイデオム
+    /// </summary>
+    struct Timer::Impl {
+        /// <summary>
+        /// コンストラクタ
+        /// </summary>
+        Impl() {}
+
+        /// <summary>
+        /// デストラクタ
+        /// </summary>
+        ~Impl() {}
+
+        /// <summary>
+        /// 時間の更新
+        /// </summary>
+        void UpdateTime() {
+            clock_gettime(CLOCK_MONOTONIC, &ts);
+        }
+
+        /// <summary>
+        /// 時間のクリア
+        /// </summary>
+        void ClearTime() {
+            ts.tv_sec = 0;
+            ts.tv_nsec = 0;
+        }
+
+        // 時間取得用
+        timespec ts;
+    };
+
+    /// <summary>
+    /// コンストラクタ
+    /// </summary>
+    Timer::Timer()
+        : impl(new Impl())
+    {}
+    Timer::Timer(const Timer& t)
+    {
+        impl = new Impl();
+        impl->ts = t.impl->ts;
+    }
+    Timer::Timer(Timer&& t)
+    {
+        impl = new Impl();
+        impl->ts = t.impl->ts;
+        impl->ClearTime();
+    }
+
+    /// <summary>
+    /// 代入演算子のオーバーライド
+    /// </summary>
+    Timer& Timer::operator=(const Timer& t)
+    {
+        if (this != &t)
+        {
+            impl->ts = t.impl->ts;
+        }
+        return *this;
+    }
+    Timer& Timer::operator=(Timer&& t)
+    {
+        if (this != &t)
+        {
+            impl->ts = t.impl->ts;
+            impl->ClearTime();
+        }
+        return *this;
+    }
+
+    /// <summary>
+    /// デストラクタ
+    /// </summary>
+    Timer::~Timer()
+    {
+        delete impl;
+    }
+
+    /// <summary>
+    /// ミリ秒を取得
+    /// </summary>
+    u64 Timer::GetMilliSec()
+    {
+        impl->UpdateTime();
+        return (u32)((impl->ts.tv_sec * 1000) + (impl->ts.tv_nsec / 1000000));
+    }
+
+    /// <summary>
+    /// マイクロ秒を取得
+    /// </summary>
+    u64 Timer::GetMicroSec()
+    {
+        impl->UpdateTime();
+        return (u32)((impl->ts.tv_sec * 1000000) + (impl->ts.tv_nsec / 1000));
+    }
+
+    /// <summary>
+    /// ナノ秒を取得
+    /// </summary>
+    u64 Timer::GetNanoSec()
+    {
+        impl->UpdateTime();
+        return (u32)((impl->ts.tv_sec * 1000000000) + impl->ts.tv_nsec);
+    }
+
+} } // namespace
--- NativeLibrary/Src/System/Timer.hpp
+++ NativeLibrary/Src/System/Timer.hpp
@@ -1,1 +1,56 @@
 #pragma once
+
+#include "Types.h"
+#include <memory>
+
+namespace drs { namespace time {
+
+    /// <summary>
+    /// タイマークラス
+    /// </summary>
+    class Timer
+    {
+    public:
+        /// <summary>
+        /// コンストラクタ
+        /// </summary>
+        Timer();
+        Timer(const Timer& t);
+        Timer(Timer&& t);
+
+        /// <summary>
+        /// デストラクタ
+        /// </summary>
+        virtual ~Timer();
+
+        /// <summary>
+        /// 代入演算子のオーバーライド
+        /// </summary>
+        Timer& operator=(const Timer& t);
+        Timer& operator=(Timer&& t);
+
+        /// <summary>
+        /// ミリ秒を取得
+        /// </summary>
+        u64 GetMilliSec();
+
+        /// <summary>
+        /// マイクロ秒を取得
+        /// </summary>
+        u64 GetMicroSec();
+
+        /// <summary>
+        /// ナノ秒を取得
+        /// </summary>
+        u64 GetNanoSec();
+
+        // PIMPLイデオム
+    private:
+        struct Impl;
+        Impl* impl;
+    protected:
+        Impl* IMPL() {
+            return impl;
+        }
+    };
+} } // namespace
